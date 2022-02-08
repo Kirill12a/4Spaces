227e55ff072562af915899a7a5ccafa7e2aed224
Kirill <45273279+Kirill12a@users.noreply.github.com>
2022-02-08 23:36:27 +0300

    Update Contents.swift

--- Contents.swift
+++ Contents.swift
@@ -22,20 +22,20 @@ protocol Shop
    - Returns: false if the product with same id already exists in the Shop, true – otherwise.
    */
   func addNewProduct(product: Product) -> Bool
-
+  
   /**
    Deletes the product with the specified id from the Shop.
    - Returns: true if the product with same id existed in the Shop, false – otherwise.
    */
   func deleteProduct(id: String) -> Bool
-
+  
   /**
    - Returns: 10 product names containing the specified string.
    If there are several products with the same name, producer's name is added to product's name in the format "<producer> - <product>",
    otherwise returns simply "<product>".
    */
   func listProductsByName(searchString: String) -> Set<String>
-
+  
   /**
    - Returns: 10 product names whose producer contains the specified string,
    result is ordered by producers.
@@ -45,10 +45,10 @@ protocol Shop
 
 // TODO: your implementation goes here
 class ShopImpl: Shop {
-
+  
   var things = [String: Product]()
-
-
+  
+  
   func addNewProduct(product: Product) -> Bool
   {
     let thingID = product.id
@@ -56,25 +56,25 @@ class ShopImpl: Shop {
     things[thingID] = product
     return true
   }
-
-
+  
+  
   func deleteProduct(id: String) -> Bool
   {
     let removeThings = things.removeValue(forKey: id)
     return removeThings == nil ? false : true
   }
-
-
+  
+  
   func listProductsByName(searchString: String) -> Set<String>
   {
     var product = Set<String>()
     let findeProduct = things.filter({$0.value.name.contains(searchString)})
-
+    
     findeProduct.values.forEach { searchProducts in
       let prod = findeProduct.filter { thing in
         thing.value.name == searchProducts.name
       }
-
+      
       if prod.count > 1
       {
         prod.forEach{
@@ -84,7 +84,7 @@ class ShopImpl: Shop {
         product.insert(searchProducts.name)
       }
     }
-
+    
     if product.count > 10
     {
       let slice = Array(product)[..<10]
@@ -92,15 +92,15 @@ class ShopImpl: Shop {
     }
     return product
   }
-
-
-
+  
+  
+  
   func listProductsByProducer(searchString: String) -> [String]
   {
     let searchResult = things.filter({$0.value.producer.contains(searchString)})
     let sortedThingsArray = searchResult.sorted(by: {$0.value.producer < $1.value.producer })
     let thingsName = sortedThingsArray.map({$0.value.name})
-
+    
     if thingsName.count > 10
     {
       let slice = thingsName[..<10]
@@ -108,9 +108,7 @@ class ShopImpl: Shop {
     }
     return thingsName
   }
-
-
-
+  //-----------\\
 }
 
 func test(lib: Shop) {
@@ -129,10 +127,10 @@ func test(lib: Shop) {
   assert(lib.addNewProduct(product: Product(id: "9", name: "Other Product9", producer: "Other Producer4")))
   assert(lib.addNewProduct(product: Product(id: "10", name: "Other Product10", producer: "Other Producer4")))
   assert(lib.addNewProduct(product: Product(id: "11", name: "Other Product11", producer: "Other Producer4")))
-
+  
   var byNames: Set<String> = lib.listProductsByName(searchString: "Product")
   assert(byNames.count == 10)
-
+  
   byNames = lib.listProductsByName(searchString: "Some Product")
   assert(byNames.count == 4)
   assert(byNames.contains("Some Producer3 - Some Product1"))
@@ -140,10 +138,10 @@ func test(lib: Shop) {
   assert(byNames.contains("Some Product3"))
   assert(!byNames.contains("Some Product1"))
   assert(byNames.contains("Some Producer1 - Some Product1"))
-
+  
   var byProducer: [String] = lib.listProductsByProducer(searchString: "Producer")
   assert(byProducer.count == 10)
-
+  
   byProducer = lib.listProductsByProducer(searchString: "Some Producer")
   assert(byProducer.count == 4)
   assert(byProducer[0] == "Some Product1")
